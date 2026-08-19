b043b38141f4f0773f53a12d7dfc0bfc4bffcd3e
thotarushitha22 <thotarushitha22@gmail.com>
2026-08-19 15:53:10 +0530

    Add SSL support for Neon

--- src/server.js
+++ src/server.js
@@ -12,7 +12,11 @@ require("./models/SellRequest");
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
