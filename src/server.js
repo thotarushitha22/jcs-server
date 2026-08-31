78a8a5c7c0b5ff002e9eb638e7741235dd8fbe67
thotarushitha22 <thotarushitha22@gmail.com>
2026-08-31 19:17:22 +0530

    Update server.js with new CORS configuration

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
     credentials: true
 }));
 
